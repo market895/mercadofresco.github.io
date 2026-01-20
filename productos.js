81ba9327ecab56fd691a0c23b1da0c289306358a
market895 <perezgallardovalentina@gmail.com>
2026-01-20 14:34:48 -0600

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,13 +1,14 @@
 const productos = [
- { nombre: "Apio", precio: null, imagen: "image/apio.jpg" },
-{ nombre: "Papa", precio: null, imagen: "image/papa.jpg" },
-{ nombre: "Zanahoria", precio: null, imagen: "image/zanahoria.jpg" },
-{ nombre: "Coliflor", precio: null, imagen: "image/coliflor.jpg" },
-{ nombre: "Lechuga", precio: null, imagen: "image/lechuga.jpg" },
-{ nombre: "Remolacha", precio: null, imagen: "image/remolacha.jpg" },
-{ nombre: "Cilantro", precio: null, imagen: "image/cilantro.jpg" },
-{ nombre: "Hierbabuena", precio: null, imagen: "image/hierbabuena.jpg" },
-{ nombre: "Perejil", precio: null, imagen: "image/perejil.jpg" }
+  { nombre: "Apio", precio: null, imagen: "image/apio.jpg" },
+  { nombre: "Papa", precio: null, imagen: "image/papa.jpg" },
+  { nombre: "Zanahoria", precio: null, imagen: "image/zanahoria.jpg" },
+  { nombre: "Coliflor", precio: null, imagen: "image/coliflor.jpg" },
+  { nombre: "Lechuga", precio: null, imagen: "image/lechuga.jpg" },
+  { nombre: "Remolacha", precio: null, imagen: "image/remolacha.jpg" },
+  { nombre: "Cilantro", precio: null, imagen: "image/cilantro.jpg" },
+  { nombre: "Hierbabuena", precio: null, imagen: "image/hierbabuena.jpg" },
+  { nombre: "Perejil", precio: null, imagen: "image/perejil.jpg" }
+];
 
 
 const contenedor = document.getElementById("productos");
